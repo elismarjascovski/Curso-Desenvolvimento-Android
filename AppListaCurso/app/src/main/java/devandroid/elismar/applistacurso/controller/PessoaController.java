e820c960b90a52397fa129d20a548e014a3c3eab
elismarjascovski <elismar@jaspes.com.br>
2023-03-29 16:25:46 -0300

    Refatorada a Classe PessoaController

--- AppListaCurso/app/src/main/java/devandroid/elismar/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/elismar/applistacurso/controller/PessoaController.java
@@ -1,13 +1,22 @@
 package devandroid.elismar.applistacurso.controller;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import devandroid.elismar.applistacurso.model.Pessoa;
+import devandroid.elismar.applistacurso.view.MainActivity;
 
 public class PessoaController {
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor listaCurso;
+    public static final String NOME_PREFERENCES = "pref_listacurso";
+    public PessoaController(MainActivity mainActivity) {
+        preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+        listaCurso = preferences.edit();
+    }
     @NonNull
     @Override
     public String toString() {
@@ -17,7 +26,28 @@ public class PessoaController {
         return super.toString();
     }
 
+    public void criarSharedPreferences() {
+    }
+
     public void salvar(Pessoa pessoa) {
-        Log.d("MVC_Controller", "Salvo: " + pessoa.toString());
+        listaCurso.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaCurso.putString("sobrenome", pessoa.getSobrenome());
+        listaCurso.putString("cursoDesejado", pessoa.getCursoDesejado());
+        listaCurso.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaCurso.apply();
+    }
+
+    public Pessoa buscar(Pessoa pessoa) {
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", ""));
+        pessoa.setSobrenome(preferences.getString("sobrenome", ""));
+        pessoa.setCursoDesejado(preferences.getString("cursoDesejado", ""));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", ""));
+
+        return pessoa;
+    }
+
+    public void limpar() {
+        listaCurso.clear();
+        listaCurso.apply();
     }
 }
